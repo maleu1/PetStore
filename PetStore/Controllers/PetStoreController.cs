a4138c8bbde8b93dabbf38133593188f35cffbe2
Martin Leuckert <martin.leuckert@neotiv.com>
2020-04-02 14:29:09 +0200

    Some comments added

--- PetStore/Controllers/PetStoreController.cs
+++ PetStore/Controllers/PetStoreController.cs
@@ -11,6 +11,7 @@ namespace PetStore.Controllers
     [Route("[controller]")]
     public class PetStoreController : ControllerBase
     {
+        // this should be replaced by a persistence layer with e.g. a database connection. 
         private List<Pet> AllPets = new List<Pet> {
             new Pet { Id = 1, Name = "Bird" }, 
             new Pet { Id = 2, Name = "Dog" }, 
@@ -21,23 +22,41 @@ namespace PetStore.Controllers
 
         private readonly ILogger<PetStoreController> _logger;
 
+        /// <summary>
+        /// Constructs a new instance of <see cref="PetStoreController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
         public PetStoreController(ILogger<PetStoreController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Returns a list of all available pets. 
+        /// </summary>
+        /// <returns>A list of pets.</returns>
         [HttpGet]
         public async Task<IEnumerable<Pet>> GetAllAsync()
         {
             return AllPets;
         }
         
+        /// <summary>
+        /// Returns a specific pet by a unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the pet.</param>
+        /// <returns>A pet.</returns>
         [HttpGet("{id}")]
         public async Task<Pet> GetPetAsync([FromRoute] int id)
         {
             return AllPets.FirstOrDefault(pet => pet.Id == id);
         }
         
+        /// <summary>
+        /// Creates a new pet.
+        /// </summary>
+        /// <param name="pet">The pet to be created.</param>
+        /// <returns>OK</returns>
         [HttpPost]
         public async Task<IActionResult> CreateNewPetAsync([FromBody] Pet pet)
         {
@@ -46,6 +65,12 @@ namespace PetStore.Controllers
             return Ok();
         }
         
+        /// <summary>
+        /// Updates a specific pet instance.
+        /// </summary>
+        /// <param name="id">Identifies the pet to be updated.</param>
+        /// <param name="name">The new name of the pet.</param>
+        /// <returns>The updated pet.</returns>
         [HttpPut("{id}")]
         public async Task<Pet> UpdatePetAsync([FromRoute] int id, [FromBody] string name)
         {
